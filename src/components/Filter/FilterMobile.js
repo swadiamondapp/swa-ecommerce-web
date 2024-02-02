3e136323c5d775808e84b7ce3a564f89ea0fd46f
anas@zinfog <anas@zinfog.com>
2024-02-02 15:10:28 +0530

    arrivals

--- src/components/Filter/FilterMobile.js
+++ src/components/Filter/FilterMobile.js
@@ -20,38 +20,43 @@ const FilterMobile = () => {
     <div>
       <div className={Classes.FilterMobiles}>
         <button onClick={showModalPrice}>Price</button>
-        <div className={Classes.PriceModal}>
-          <Modal
-            title="Price"
-            open={isModalOpen}
-            onOk={handleOkPrice}
-            onCancel={handleCancelPrice}
-          >
-            <div className={Classes.PriceTgs}>
-              <div className={Classes.PriceTags1}>
-                <input type="checkbox" />
-                <label>above 50000</label>
-              </div>
-              <div className={Classes.PriceTags1}>
-                <input type="checkbox" />
-                <label>20000 to 30000</label>
-              </div>
-              <div className={Classes.PriceTags1}>
-                <input type="checkbox" />
-                <label>30000 to 50000</label>
-              </div>
 
-              <div className={Classes.PriceTags1}>
-                <input type="checkbox" />
-                <label>less than 10000</label>
-              </div>
-            </div>
-          </Modal>
-        </div>
         <div className={Classes.arrow1}></div>
         <button>Sort</button>
+
         <div className={Classes.arrow1}></div>
         <button>Filter</button>
+      </div>
+      <div className={Classes.PriceModal}>
+        <Modal
+          title="Price"
+          open={isModalOpen}
+          onOk={handleOkPrice}
+          onCancel={handleCancelPrice}
+        >
+          <div className={Classes.PriceTgs}>
+            <div className={Classes.PriceTags1}>
+              <input type="checkbox" />
+              <label>above 50000</label>
+            </div>
+            <div className={Classes.PriceTags1}>
+              <input type="checkbox" />
+              <label>20000 to 30000</label>
+            </div>
+            <div className={Classes.PriceTags1}>
+              <input type="checkbox" />
+              <label>30000 to 50000</label>
+            </div>
+
+            <div className={Classes.PriceTags1}>
+              <input type="checkbox" />
+              <label>less than 10000</label>
+            </div>
+          </div>
+          <div className={Classes.PriceBtns}>
+            <button>Done</button>
+          </div>
+        </Modal>
       </div>
     </div>
   );
