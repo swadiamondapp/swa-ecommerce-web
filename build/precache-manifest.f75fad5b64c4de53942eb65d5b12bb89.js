426d1436a9cda09a1d8ea255381d7efa6422f3e3
Nithin2255 <nithin@zinfog.com>
2024-02-02 15:18:06 +0530

    changes

---
+++ build/precache-manifest.f75fad5b64c4de53942eb65d5b12bb89.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c14561b1499aa8dc476",
+    "url": "/static/css/main.4bdbca52.chunk.css"
+  },
+  {
+    "revision": "5c14561b1499aa8dc476",
+    "url": "/static/js/main.5c14561b.chunk.js"
+  },
+  {
+    "revision": "571abc403a731f4447bb",
+    "url": "/static/js/1.571abc40.chunk.js"
+  },
+  {
+    "revision": "80711d665a670ca95a66",
+    "url": "/static/css/2.38cc6988.chunk.css"
+  },
+  {
+    "revision": "80711d665a670ca95a66",
+    "url": "/static/js/2.80711d66.chunk.js"
+  },
+  {
+    "revision": "a667b975987d1624b73a",
+    "url": "/static/js/runtime~main.a667b975.js"
+  },
+  {
+    "revision": "8bc6d3508a6694dd7ba68d5bb9a0ce69",
+    "url": "/static/media/downBg.8bc6d350.png"
+  },
+  {
+    "revision": "77e6818763c8183d6e40c9134309250d",
+    "url": "/static/media/ShopOnBudget1.77e68187.png"
+  },
+  {
+    "revision": "9e9bb02a58447276f2e53d4343bdcdb7",
+    "url": "/static/media/ShopOnBudget2.9e9bb02a.png"
+  },
+  {
+    "revision": "7fecd4e345dccd46f0478cd80c04550d",
+    "url": "/static/media/ShopOnBudget3.7fecd4e3.png"
+  },
+  {
+    "revision": "27a1f607e1c8d1d9d631e3f519aeddb6",
+    "url": "/static/media/ShopOnBudget4.27a1f607.png"
+  },
+  {
+    "revision": "bf8cac0a1e363689049f569f63bed7a5",
+    "url": "/static/media/Certificate.bf8cac0a.png"
+  },
+  {
+    "revision": "02373ad4ff7c60c1b69edd2216717f6c",
+    "url": "/static/media/empwish.02373ad4.png"
+  },
+  {
+    "revision": "b8cc3851f7e18fae66f393a55a4e0d79",
+    "url": "/static/media/cartempty.b8cc3851.png"
+  },
+  {
+    "revision": "79e0869456478cc9cd857f2618835ead",
+    "url": "/static/media/new1.79e08694.png"
+  },
+  {
+    "revision": "31ff7c1a62a300dbbf9656b4ba14a0d5",
+    "url": "/static/media/Gilroy-Regular.31ff7c1a.ttf"
+  },
+  {
+    "revision": "a96ecd13655587d30a21265c547cd8aa",
+    "url": "/static/media/PlayfairDisplay-Regular.a96ecd13.ttf"
+  },
+  {
+    "revision": "5d08cc9fffd565a2b9d9baae3846cb65",
+    "url": "/static/media/Gilroy-MediumItalic.5d08cc9f.ttf"
+  },
+  {
+    "revision": "079af0e2936ccb99b391ddc0bbb73dcb",
+    "url": "/static/media/Inter-Regular.079af0e2.ttf"
+  },
+  {
+    "revision": "aea49ef97279a073a40c509cf817004d",
+    "url": "/static/media/DownloadOurApp.aea49ef9.png"
+  },
+  {
+    "revision": "1de4d7bdef911278eb3d9204db8fcd12",
+    "url": "/static/media/new4.1de4d7bd.png"
+  },
+  {
+    "revision": "964f2bd86d33f0ed835180fd75913b5d",
+    "url": "/static/media/Ringflip.964f2bd8.png"
+  },
+  {
+    "revision": "2530d38f777a1d118016aed7dc6d3977",
+    "url": "/static/media/RingRotate.2530d38f.png"
+  },
+  {
+    "revision": "07ed14bd1f3a6f52ad8e6f7eb54d826d",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6f12ad5af3529b686c81b1da5c90a5e1.js"
+  "/precache-manifest.f75fad5b64c4de53942eb65d5b12bb89.js"
 );
 
 workbox.clientsClaim();
