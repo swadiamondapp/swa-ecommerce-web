9f4a1482dbde84ccb170a1978f0c83e344dd65c6
Nithin2255 <nithin@zinfog.com>
2024-02-07 15:46:54 +0530

    changes

---
+++ src/Pages/PaymentPage/PaymentPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Classes from "./PaymentPage.module.css";
+import Header from "../../components/Header/Header";
+import Features from "../../components/Features/Features";
+import Footer from "../../components/Footer/Footer";
+import Payment from "../../components/Payment/Payment";
+
+const PaymentPage = (props) => {
+  return (
+    <div>
+      <div className={Classes.Background}>
+        <Header />
+        <Payment />
+        <div className={Classes.Features}>
+          <Features />
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default PaymentPage;
