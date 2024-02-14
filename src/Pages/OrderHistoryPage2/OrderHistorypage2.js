5f94e1cac14458b7cb64e71e01603fa4aac72948
anas@zinfog <anas@zinfog.com>
2024-02-14 19:56:49 +0530

    track

--- src/Pages/OrderHistoryPage2/OrderHistorypage2.js
+++ src/Pages/OrderHistoryPage2/OrderHistorypage2.js
@@ -5,7 +5,9 @@ import Features from "../../components/Features/Features";
 import OrderHistory from "../../components/OrderHistory/OrderHistory";
 import OrderHistoryCard2 from "../../components/OrderHistory/OrderHistoryCard2/OrderHistoryCard2";
 import ProductImage from "../../Assets/pandant chain 1.png";
-import { Steps } from "antd";
+import deliveryimg from "../../Assets/delivery.png";
+import imgproduct from "../../Assets/diamonds.png";
+import { Steps, Collapse } from "antd";
 import TrackOrder from "../../components/OrderHistory/TrackOrder/TrackOrder";
 import Classes from "./OrderHistoryPage2.module.css";
 import { useHistory } from "react-router-dom";
@@ -50,39 +52,52 @@ const OrderHistorypage2 = (props) => {
   const [payMode, setPayMode] = useState("");
   const [orderPlaced, setOrderPlaced] = useState("");
   const token = localStorage.getItem("swaToken");
-  useEffect(() => {
-    axios
-      .get(Urls.myOrder + "/" + props.location.state.data, {
-        headers: { Authorization: "Token " + token },
-      })
-      .then((response1) => {
-        setAddress(response1.data.results.data.order.address);
-        setOrderId(response1.data.results.data.order.order_code);
-        setOrderPlaced(response1.data.results.data.order.order_at);
-        setTotal(response1.data.results.data.order.grand_total);
-        setPromoCode(response1.data.results.data.order.promocode);
-        setSubTot(response1.data.results.data.order.orders_total);
-        setOrderDet(response1.data.results.data.order.shipment);
-        setPayMode(response1.data.results.data.order.payment_mode);
-        setDoctNum(response1.data.results.data.order.shipment[0].docket_number);
-        
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    axios
-      .get(Urls.cart, { headers: { Authorization: "Token " + token } })
-      .then((response1) => {
-        if (response1.data.results.message === "cart is empty") {
-          setCartCount("");
-        } else {
-          setCartCount(response1.data.results.count);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  const { Panel } = Collapse;
+
+  const onChange = (key) => {
+    console.log(key);
+  };
+  // warnning
+
+  // useEffect(() => {
+  //   axios
+  //     .get(Urls.myOrder + "/" + props.location.state.data, {
+  //       headers: {
+  //         Authorization: "Token " + token,
+  //       },
+  //     })
+  //     .then((response1) => {
+  //       setAddress(response1.data.results.data.order.address);
+  //       setOrderId(response1.data.results.data.order.order_code);
+  //       setOrderPlaced(response1.data.results.data.order.order_at);
+  //       setTotal(response1.data.results.data.order.grand_total);
+  //       setPromoCode(response1.data.results.data.order.promocode);
+  //       setSubTot(response1.data.results.data.order.orders_total);
+  //       setOrderDet(response1.data.results.data.order.shipment);
+  //       setPayMode(response1.data.results.data.order.payment_mode);
+  //       setDoctNum(response1.data.results.data.order.shipment[0].docket_number);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  //   axios
+  //     .get(Urls.cart, {
+  //       headers: {
+  //         Authorization: "Token " + token,
+  //       },
+  //     })
+  //     .then((response1) => {
+  //       if (response1.data.results.message === "cart is empty") {
+  //         setCartCount("");
+  //       } else {
+  //         setCartCount(response1.data.results.count);
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }, []);
+  // warnning
 
   const homeHandler = () => {
     history.pushState("/");
@@ -92,8 +107,12 @@ const OrderHistorypage2 = (props) => {
   };
   const rateRevHandler = (proId) => {
     console.log(proId);
-    history.push({ pathname: "/rate_review", state: { data: proId } });
+    history.push({
+      pathname: "/rate_review",
+      state: { data: proId },
+    });
   };
+
   return (
     <div>
       <div className={Classes.Background}>
@@ -103,25 +122,43 @@ const OrderHistorypage2 = (props) => {
           <div className="container">
             <div className="container">
               <div className={Classes.Main}>
-                <h1 className={Classes.Title}>Shipment Details</h1>
-                <div className={Classes.SubText}>
+                {/* <h1 className={Classes.Title}>Shipment Details</h1> */}
+                <h3 className={Classes.orderidh3}>
+                  Order ID : SWA4R46RF46R356F45
+                </h3>
+                <div className={Classes.DeliveryDetails}>
+                  <p>
+                    <img src={deliveryimg} />
+                    Delivered on <span>26 may 2023</span>
+                  </p>
+                </div>
+                {/* <div className={Classes.SubText}>
                   <p
                     className={`${Classes.Home} ${Classes.HomeNew}`}
                     onClick={homeHandler}
-                    style={{ cursor: "pointer" }}
+                    style={{
+                      cursor: "pointer",
+                    }}
                   >
                     HOME /
                   </p>
                   <p
                     className={`${Classes.Home} ${Classes.HomeNew}`}
                     onClick={orderHistory}
-                    style={{ cursor: "pointer" }}
+                    style={{
+                      cursor: "pointer",
+                    }}
                   >
                     ORDER HISTORY/
                   </p>
                   <p className={Classes.NewArrival}>SHIPMENT DETAILS</p>
-                </div>
-                <div className="d-flex" style={{ paddingBottom: "20px" }}>
+                </div> */}
+                {/* <div
+                  className="d-flex"
+                  style={{
+                    paddingBottom: "20px",
+                  }}
+                >
                   <div className={Classes.head}>
                     Order placed on{" "}
                     <span className={Classes.date}>
@@ -132,10 +169,193 @@ const OrderHistorypage2 = (props) => {
                   <div className={Classes.head}>
                     Order ID <span className={Classes.date}>{orderId} </span>
                   </div>
-                </div>
+                </div> */}
+              </div>
+              {/* new design */}
+              <div className="item_collapse" style={{ margin: "10px 50px" }}>
+                <Collapse
+                  onChange={onChange}
+                  style={{ ["--header-border"]: "" }}
+                >
+                  {/* Panel 1: Shipping Address */}
+                  <Panel
+                    header="Shipping Address"
+                    key="shipping"
+                    style={{
+                      color: "#0D1217",
+                      fontSize: "16px",
+                      fontFamily: "Lato",
+                      fontWeight: 400,
+                    }}
+                  >
+                    <div className={Classes.ShippingDetialHead}>
+                      <p className={Classes.nameShipment}>Mohammed Inshad</p>
+                      <div className={Classes.AddressShipping}>
+                        <p>
+                          Kottakunnan ( house ) morayur 673642 <br /> opposit
+                          family health center <br /> malappuram district <br />{" "}
+                          kerala 673643
+                        </p>
+                        <p className={Classes.phoneSh}>
+                          Phone number : 9995200745
+                        </p>
+                      </div>
+                    </div>
+                  </Panel>
+
+                  {/* Panel 2: Payment Method */}
+                  <Panel
+                    header="Product Details"
+                    key="payment"
+                    style={{
+                      color: "#0D1217",
+                      fontSize: "16px",
+                      fontFamily: "Lato",
+                      fontWeight: 500,
+                    }}
+                  >
+                    <div className={Classes.ProductDetailsParent1}>
+                      <div className={Classes.LftProductDetail}>
+                        <img src={imgproduct} />
+                        <div>
+                          <p className={Classes.PDiamond1}>Diamond ring</p>
+                          <p style={{ color: "#757C81" }}>
+                            18 KT yellow gold 12.460 GM
+                          </p>
+                          <p style={{ color: "#757C81" }}>
+                            Diamond 0.680 Carat SIIJ
+                          </p>
+                          <p style={{ color: "#303A42" }}>SKU 1245</p>
+                        </div>
+                      </div>
+                      <div className={Classes.rgtProductDetails}>
+                        <p>Qty 1</p>
+                      </div>
+                    </div>
+                    <div className={Classes.crtn1}>
+                      <p>
+                        Certification No :{" "}
+                        <span style={{ color: "#0997E7" }}>SGLJMH6786532E</span>
+                      </p>
+                    </div>
+                  </Panel>
+
+                  {/* Panel 3: Order Summary */}
+                  <Panel
+                    header="Payment Details"
+                    key="summary"
+                    style={{
+                      color: "#0D1217",
+                      fontSize: "16px",
+                      fontFamily: "Lato",
+                      fontWeight: 400,
+                    }}
+                  >
+                    <div className={Classes.parentPaymentItems}>
+                      <div className={Classes.PaymentItems}>
+                        <p>Item Subtotal</p>
+                        <p>27000</p>
+                      </div>
+                      <div className={Classes.PaymentItems}>
+                        <p>Shipping</p>
+                        <p>0.00</p>
+                      </div>
+                      <div className={Classes.PaymentItems}>
+                        <p>Total</p>
+                        <p>27000</p>
+                      </div>
+                      <div className={Classes.PaymentItems}>
+                        <p>Promo code</p>
+                        <p style={{ color: "#000000" }}>
+                          PAYDAY{" "}
+                          <span style={{ color: "#30933A" }}>Applied</span>
+                        </p>
+                      </div>
+                      <div className={Classes.PaymentItems}>
+                        <p>Coupon Discount</p>
+                        <p style={{ color: "#000000" }}>2500</p>
+                      </div>
+                      <div className={Classes.PaymentItems}>
+                        <p style={{ color: "#000000" }}>Payable</p>
+                        <p style={{ color: "#000000" }}>24500</p>
+                      </div>
+                    </div>
+                  </Panel>
+                  <Panel
+                    header="Track order"
+                    key="track"
+                    style={{
+                      color: "#0D1217",
+                      fontSize: "16px",
+                      fontFamily: "Lato",
+                      fontWeight: 400,
+                    }}
+                  >
+                    <div className={Classes.ParentStatus}>
+                      <div className={Classes.leftStatus1}>
+                        <div className={Classes.leftStatus2}>
+                          <div className={Classes.dotstatus}></div>
+                          <div className={Classes.dotstatusline}></div>
+                        </div>
+                        <div className={Classes.leftStatus2}>
+                          <div className={Classes.dotstatus1}></div>
+                          <div className={Classes.dotstatusline1}></div>
+                        </div>
+                        <div className={Classes.leftStatus2}>
+                          <div className={Classes.dotstatus1}></div>
+                          <div className={Classes.dotstatusline1}></div>
+                        </div>
+                        <div className={Classes.leftStatus2}>
+                          <div className={Classes.dotstatus1}></div>
+                          <div className={Classes.dotstatusline1}></div>
+                        </div>
+                      </div>
+                      <div className={Classes.rightStatus1}>
+                        <div className={Classes.RightStausshow}>
+                          <p className={Classes.RsHead}>Order confirmed</p>
+                          <p style={{ color: "#A3A7AB" }}>
+                            Tues 18 oct’2022 , 4:45 PM
+                          </p>
+                        </div>
+                        <div className={Classes.RightStausshow}>
+                          <p
+                            style={{ color: "#A3A7AB" }}
+                            className={Classes.RsHead}
+                          >
+                            Shipped
+                          </p>
+                          <p style={{ color: "#A3A7AB" }}>
+                            Expected by Friday 19th Oct
+                          </p>
+                        </div>
+                        <div className={Classes.RightStausshow2}>
+                          <p
+                            style={{ color: "#A3A7AB" }}
+                            className={Classes.RsHead}
+                          >
+                            Out of delivery
+                          </p>
+                          <p style={{ color: "#A3A7AB" }}></p>
+                        </div>
+                        <div className={Classes.RightStausshow3}>
+                          <p
+                            style={{ color: "#A3A7AB" }}
+                            className={Classes.RsHead}
+                          >
+                            Delivery
+                          </p>
+                          <p style={{ color: "#A3A7AB" }}></p>
+                        </div>
+                      </div>
+                    </div>
+                  </Panel>
+                </Collapse>
               </div>
 
-              <div className={Classes.shipMent}>
+              {/* new design */}
+
+              {/* warrning **** */}
+              {/* <div className={Classes.shipMent}>
                 <div className="row">
                   <div className="col-md-4 vl">
                     <div className={Classes.head}>Shipping details </div>
@@ -161,21 +381,20 @@ const OrderHistorypage2 = (props) => {
                       <p className={Classes.Adres}>Item Subtotal</p>
                       <div className={Classes.Adres}>{subTot}</div>
                     </div>
-                    {/* <div className={Classes.Box}>
-                      <p className={Classes.Adres}>Shipping</p>
-                      <div className={Classes.Adres}>2000</div>
-                    </div> */}
-                    {/* <div className={Classes.Box}>
-                      <p className={Classes.Adres}>Total</p>
-                      <div className={Classes.Adres}>2000</div>
-                    </div> */}
+                    
                     <div className={Classes.Box}>
                       <p className={Classes.Adres}>Promo code</p>
                       <div className={Classes.Adres}>
                         {promoCode === null ? (
                           <span>Not Applied</span>
                         ) : (
-                          <span style={{ color: "#30933A" }}>Applied</span>
+                          <span
+                            style={{
+                              color: "#30933A",
+                            }}
+                          >
+                            Applied
+                          </span>
                         )}
                       </div>
                     </div>
@@ -186,14 +405,18 @@ const OrderHistorypage2 = (props) => {
                       </div>
                     ) : null}
 
-                    <div className={Classes.Box} style={{ fontWeight: "600" }}>
+                    <div
+                      className={Classes.Box}
+                      style={{
+                        fontWeight: "600",
+                      }}
+                    >
                       <p className={Classes.Adres}>Grand Total</p>
                       <div className={Classes.Adres}>{total}</div>
                     </div>
                   </div>
                 </div>
               </div>
-
               <div className={Classes.Products}>
                 {orderDet.map((item, index) => {
                   return (
@@ -216,11 +439,12 @@ const OrderHistorypage2 = (props) => {
                       qty={item.quantity}
                       clicked={() => rateRevHandler(item.product.product_id)}
                     >
-                      <TrackOrder doctNum = {doctNum}/>
+                      <TrackOrder doctNum={doctNum} />
                     </OrderHistoryCard2>
                   );
                 })}
-              </div>
+              </div> */}
+              {/* warrning **** */}
             </div>
           </div>
         </div>
