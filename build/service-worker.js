836de42deaec89c65686741bf715b454eb88a983
Nithin2255 <nithin@zinfog.com>
2024-02-29 18:04:20 +0530

    changes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.826a4fc03368708d60da81726fdc1f06.js"
+  "/precache-manifest.cdabe9673b25fbff247beae44864cf4a.js"
 );
 
 workbox.clientsClaim();
