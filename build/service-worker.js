077d3097511eb030c7e0b1c711efff60ba741070
sabiemuhammed <sabeeh@zinfog.com>
2024-04-20 19:01:56 +0530

    latest_Build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.68c1a4ef8bc8f11b355495cc63adc482.js"
+  "/precache-manifest.97429989133a77d15fae15bccf03877d.js"
 );
 
 workbox.clientsClaim();
