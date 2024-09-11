67d27e4d4cb9e423b2d03e6cda693a5bc86c28e5
sayyan <sayyan@zinfog.com>
2024-09-11 16:58:37 +0530

    changes

--- src/components/CartDesign/CartDesign.js
+++ src/components/CartDesign/CartDesign.js
@@ -369,9 +369,13 @@ function CartDesign(props) {
                         <p className={Classes.TotalSmall}>
                           Total &nbsp;
                           <span>
-                            (
+                            {/* (
                             {props.tryCartcountResults &&
                               props.tryCartcountResults.item_count}{" "}
+                            Items) */}
+                            (
+                            {props.cartCount &&
+                              props.cartCount}{" "}
                             Items)
                           </span>
                         </p>
