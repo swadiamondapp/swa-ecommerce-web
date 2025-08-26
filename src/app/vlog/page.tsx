214ab2478c220aa0dc38a59babf42aad927c0b8d
basithallied <43291406+basithallied@users.noreply.github.com>
2025-08-26 09:59:22 +0530

    Update page.tsx

--- src/app/vlog/page.tsx
+++ src/app/vlog/page.tsx
@@ -12,8 +12,3 @@ export default function VlogPage() {
   );
 }
 
-// app/vlog/layout.tsx
-export default function VlogLayout({ children }: { children: React.ReactNode }) {
-  // Return children without wrapping it in your main layout
-  return <>{children}</>;
-}
