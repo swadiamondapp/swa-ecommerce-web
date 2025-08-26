1745537ccaab3c6989c4bb573277690d756a9626
basithallied <43291406+basithallied@users.noreply.github.com>
2025-08-26 10:38:51 +0530

    Create page.js

---
+++ src/app/vlog/page.js
@@ -0,0 +1,25 @@
+'use client';
+import { useEffect, useState } from 'react';
+
+export default function VlogPage() {
+  const [content, setContent] = useState('<p>Loading...</p>');
+
+  useEffect(() => {
+    async function loadBlog() {
+      try {
+        const res = await fetch('/api/vlog-proxy');
+        const html = await res.text();
+        setContent(html);
+      } catch {
+        setContent('<p>Error loading blog.</p>');
+      }
+    }
+    loadBlog();
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-white">
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </div>
+  );
+}
