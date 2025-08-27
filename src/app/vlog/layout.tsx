2619bbd417cfacfb14b0acd6a8d3db96511ff543
basithallied <43291406+basithallied@users.noreply.github.com>
2025-08-27 11:14:39 +0530

    Delete src/app/vlog/layout.tsx

--- src/app/vlog/layout.tsx
+++
@@ -1,9 +0,0 @@
-//src/app/vlog/layout.tsx
-export default function VlogLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  // This overrides your main layout so NO header/footer
-  return <>{children}</>;
-} 
