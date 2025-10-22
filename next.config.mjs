a52ac338add01cbe48173f266ff21c5929af2469
basithallied <43291406+basithallied@users.noreply.github.com>
2025-10-22 17:56:24 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 👇 This is the key line for static export (replaces `next export`)
+  output: 'export',
+
   images: {
     remotePatterns: [
-      {
-        hostname: "swaecommain.swa.co",
-      },
+      { hostname: "swaecommain.swa.co" },
       { hostname: "swaecomordermain.swa.co" },
       { hostname: "swaordernewtest.zinfog.in" },
       { hostname: "swadiamonds.com" },
     ],
     unoptimized: true,
   },
+
   experimental: {
     optimizePackageImports: ["@radix-ui/react-icons"],
   },
