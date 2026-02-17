8a6a14df9eb3a2fd09e9887719efd909acaa03c5
Sumesh Sivan <sumeshshivan@gmail.com>
2026-02-17 15:43:49 +0530

    Add Meta Pixel tracking code and noscript fallback to layout for enhanced analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -121,6 +121,22 @@ export default function RootLayout({ children }) {
           `}
         </Script>
 
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '639310924495116');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
         {/* GTM Script */}
         <Script
           id="gtm-script"
@@ -146,6 +162,17 @@ export default function RootLayout({ children }) {
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
+        </noscript>
+
+        {/* Meta Pixel noscript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=639310924495116&ev=PageView&noscript=1"
+            alt=""
+          />
         </noscript>
         <QueryProvider>
           <AuthProvider>
