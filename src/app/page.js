73623b7a6f89c426de9d773bf5b2b35fdbb7cf24
swadiamondapp <swadiamondapp@gmail.com>
2026-05-17 07:02:46 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,55 @@
-export default async function Home() {
+"use client";
+
+export default function Home() {
   return (
-    <div className="maintenance-page">
-      
-      <div className="diamond"></div>
-      <div className="diamond"></div>
-      <div className="diamond"></div>
-      <div className="diamond"></div>
+    <div className="maintenance-wrapper">
 
-      <div className="container">
+      {/* Animated Background */}
+      <div className="bg-glow glow1"></div>
+      <div className="bg-glow glow2"></div>
 
-        <div className="logo">
-          SWA <span>DIAMONDS</span>
+      {/* Floating Diamonds */}
+      <span className="floating floating1"></span>
+      <span className="floating floating2"></span>
+      <span className="floating floating3"></span>
+      <span className="floating floating4"></span>
+
+      {/* Main Card */}
+      <div className="maintenance-card">
+
+        <div className="diamond-logo">
+          ◆
         </div>
 
-        <h1>Website Under Maintenance</h1>
+        <h2 className="brand-name">
+          SWA DIAMONDS
+        </h2>
 
-        <p>
-          We are crafting something brilliant for you.
-          Our website is currently undergoing scheduled maintenance.
-          We’ll be back shortly with an enhanced luxury experience.
+        <h1 className="title">
+          We’re Upgrading Something Beautiful
+        </h1>
+
+        <p className="description">
+          Our website is currently under scheduled maintenance
+          to deliver a faster, more luxurious shopping experience.
+          We’ll be back shortly.
         </p>
 
-        <a href="#" className="btn">
-          Coming Soon
-        </a>
+        {/* Loader */}
+        <div className="luxury-loader">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* Contact */}
+        <div className="contact-box">
+          <p>Need assistance?</p>
+
+          <a href="mailto:info@swadiamonds.com">
+            info@swadiamonds.com
+          </a>
+        </div>
 
       </div>
 
