489d6b50e89ee2d3881114f2e822e6a3d84f2c5e
swadiamondapp <swadiamondapp@gmail.com>
2026-05-17 06:36:46 +0530

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,32 @@
+export default async function Home() {
+  return (
+    <div className="maintenance-page">
+      
+      <div className="diamond"></div>
+      <div className="diamond"></div>
+      <div className="diamond"></div>
+      <div className="diamond"></div>
+
+      <div className="container">
+
+        <div className="logo">
+          SWA <span>DIAMONDS</span>
+        </div>
+
+        <h1>Website Under Maintenance</h1>
+
+        <p>
+          We are crafting something brilliant for you.
+          Our website is currently undergoing scheduled maintenance.
+          We’ll be back shortly with an enhanced luxury experience.
+        </p>
+
+        <a href="#" className="btn">
+          Coming Soon
+        </a>
+
+      </div>
+
+    </div>
+  );
+}
