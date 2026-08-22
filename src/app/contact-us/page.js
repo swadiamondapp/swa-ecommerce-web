27c79de224a070c3054259961cbdae41293faf18
swadiamondapp <swadiamondapp@gmail.com>
2026-08-22 12:34:31 +0530

    Update page.js

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Contactus from './Contact-us'
-
+export const metadata = { 
+  title: "SWA Diamonds Customer Care Number | Customer Care Service & Contact", 
+  description: "Looking for the SWA Diamonds customer care number or customer care service? Contact SWA Diamonds for assistance with jewellery orders, products, services, support and enquiries.", 
+};
 function Contact() {
   return (
     <Contactus/>
